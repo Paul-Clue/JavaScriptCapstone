58c02346dd30d19364bc585bc5b78136f396193e
unknown <paulclue20@gmail.com>
2021-06-27 08:08:07 -0500

    Add configuration

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,35 @@
+import 'phaser';
+ 
+export default class GameScene extends Phaser.Scene {
+  constructor () {
+    super('Game');
+  }
+ 
+  preload () {
+    // load images
+    this.load.image('logo', 'assets/logo.png');
+  }
+ 
+  create () {
+    this.add.image(400, 300, 'logo');
+  }
+};
+
+
+
+
+
+// export class SimpleScene extends Phaser.Scene {
+//   create() {
+//     this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+//   }
+
+//   preload() {
+//     this.load.image('cokecan', 'assets/pimg.png');
+//   }
+
+//   create() {
+//     this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
+//     this.add.image(100, 200, 'cokecan');
+//   }
+// }
